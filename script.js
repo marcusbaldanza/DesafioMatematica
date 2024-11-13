36208a5444f279b29f63df1d260b22eb655e7b7c
marcusbaldanza <marcusbaldanza@gmail.com>
2024-11-13 10:39:26 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ let interval;
 let correctAnswers = 0;
 let pointsPerCorrect = 5;
 let pointsPerMistake = 3;
-let maxRandomNumber = 10;
+let maxRandomNumber = 5;
 let gameActive = true;
 
 function generateRandomNumber(max) {
@@ -79,7 +79,7 @@ function checkAnswer(selected, correct) {
         }
         
         if (correctAnswers % 5 === 0) {
-            maxRandomNumber += 10;
+            maxRandomNumber += 5;
         }
 
         scoreDiv.textContent = `Pontos: ${score}`;
@@ -129,8 +129,30 @@ function endGame() {
     clearInterval(interval);
     gameActive = false;
 
-    // Exibir o quadrante "Jogo Finalizado" ao terminar o jogo
+    // Exibir pontuação final
     finalScoreSpan.textContent = score;
+    
+    // Exibir mensagem cômica com base na pontuação
+    const finalMessage = document.getElementById("final-message");
+    if (score <= 0) {
+        finalMessage.textContent = "Caramba, você sabe o que é matemática? Medo de você precisar trabalhar com números!";
+    } else if (score > 0 && score <= 30) {
+        finalMessage.textContent = "Fugiu da escola? Precisa estudar mais, matemática salva vidas!";
+    } else if (score > 30 && score <= 60) {
+        finalMessage.textContent = "Vamos praticar mais, assim vão te fazer de bobo(a) até em troco de padaria!";
+    } else if (score > 60 && score <= 90) {
+        finalMessage.textContent = "Sinto que neurônios foram fritados mas precisa tirar mais o ferrugem, continue praticando!";
+    } else if (score > 90 && score <= 120) {
+        finalMessage.textContent = "Olha só alguém metido a espertinho, ta ficando bom só que ainda é pouco!";
+    } else if (score > 120 && score <= 150) {
+        finalMessage.textContent = "Tá quase dando gosto de ver, mas ainda acho que se espremer mais um pouco você melhora!";
+    } else if (score > 150 && score <= 180) {
+        finalMessage.textContent = "Olha, um alguém que eu quase posso considerar um rival, aparentemente anda praticando!";
+    } else if (score > 180) {
+        finalMessage.textContent = "Muito bem, gostei de ver, alguém que aprecia a matemática como deve ser, pode ser meu discípulo(a)!";
+    }
+
+    // Exibir o quadrante "Jogo Finalizado" ao terminar o jogo
     gameOverDiv.classList.remove('hidden');
 }
 
